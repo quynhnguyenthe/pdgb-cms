3dd80331d8b304b70c279f1d426daae6812a8b85
Nguyen The Quynh <quynh.nguyenthe@amela.vn>
2023-11-29 22:02:43 +0700

    add key request club for api user info

--- app/Http/Controllers/Api/User/MemberController.php
+++ app/Http/Controllers/Api/User/MemberController.php
@@ -3,19 +3,28 @@
 namespace App\Http\Controllers\Api\User;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\ClubRequestRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MemberController extends Controller
 {
-    public function __construct()
-    {
+    /**
+     * @var ClubRequestRepository
+     */
+    private $clubRequestRepository;
 
+    public function __construct(
+        ClubRequestRepository $clubRequestRepository
+    )
+    {
+        $this->clubRequestRepository = $clubRequestRepository;
     }
 
     public function userInfo()
     {
         $user = Auth::guard('google-member')->user();
+        $user['request_club'] = $this->clubRequestRepository->getClubRquestWithMember($user->id);
 
         return response()->json(['message' => 'success', 'data' => $user], 200);
     }
---
+++ database/migrations/2023_11_29_142729_create_match_results_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('match_results', function (Blueprint $table) {
+            $table->id();
+            $table->integer('match_id');
+            $table->integer('creator_id');
+            $table->integer('acceptor_id');
+            $table->integer('win_team_id');
+            $table->integer('lose_team_id');
+            $table->string('result');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('match_results');
+    }
+}
