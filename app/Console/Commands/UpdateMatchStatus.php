f1b445be2916f1a37bf2f163da3a0abe3fc01630
QuynhNT <quynh.nguyenthe@amela.vn>
2023-12-01 16:31:42 +0700

    add cronjob matches

--- app/Console/Commands/UpdateMatchStatus.php
+++ app/Console/Commands/UpdateMatchStatus.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Matches;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -38,7 +39,18 @@ class UpdateMatchStatus extends Command
      */
     public function handle()
     {
-        DB::table('matches')->get();
-        echo showCli('Success');
+        $date = date('Y-m-d');
+        $time = date('H:i:s');
+        $dateTime = date('Y-m-d H:i:s');
+        $matches = DB::table('matches')
+            ->where('match_date', '<=', $date)
+            ->where('match_time', '<=',$time)
+            ->where('status', Matches::STATUS_ACCEPTED)
+            ->update(['status' => Matches::STATUS_IN_DUE]);
+        DB::table('matches')
+            ->where('match_end_date', '<=', $dateTime)
+            ->where('status', Matches::STATUS_IN_DUE)
+            ->update(['status' => Matches::WAIT_RESULT]);
+        echo "\033[01;33mSuccess\033[0m" . PHP_EOL;;
     }
 }
